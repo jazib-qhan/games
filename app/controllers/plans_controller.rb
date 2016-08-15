c258bff9a88c955590415dab088cfd036b8cb7e9
Saqib R <railshub@gmail.com>
2016-08-15 14:12:31 +0500

    - Add future plans on plans page.

---
+++ app/controllers/plans_controller.rb
@@ -0,0 +1,12 @@
+##
+# Public class which renders the Whats my future plans
+# in the application.
+
+
+class PlansController < ApplicationController
+
+  respond_to :html, :json
+
+  def index
+  end
+end
